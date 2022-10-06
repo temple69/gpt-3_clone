6bd6ed4cf264008aa4b9d96ad6c1929ab2dbe99e
temple <utemple169@gmail.com>
2022-10-06 15:20:47 +0100

    made nav tablet responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,7 +15,12 @@ const Header = () => {
     <header className={classes.header}>
          <ul className={classes.mobile_design}>
           <li><strong>GPT-3</strong></li>
-          <button onClick={browserHandler}>{toggle?"-":"+"}</button>
+          <button onClick={browserHandler} >{toggle?"-":"+"}</button>
+          </ul> 
+          <ul className={classes.tablet_design}>
+          <li><strong>GPT-3</strong></li>
+          <button onClick={browserHandler} className={classes.button2}>{toggle?"-":"+"}</button>
+          <button>Sign up</button>
           </ul> 
         <nav>
           <ul>
@@ -31,7 +36,6 @@ const Header = () => {
             {/* Mobile responsive design */}
             {toggle&&(
             <ul className={toggle?classes.anime:''}>
-                <li><strong>GPT-3</strong></li>
                   <li><a href="#">Home</a></li>
                   <li><a href="#">What is GPT?</a></li>
                   <li><a href="#">Open AI</a></li>
@@ -39,6 +43,16 @@ const Header = () => {
                   <li><a href="#">Library</a></li>
                   <li><a href="#">Sign in</a></li>
                   <button><a href="#">Sign up</a></button>
+            </ul>)}
+            {/* Tablet responsiveness */}
+            {toggle&&(
+            <ul className={toggle?classes.anime2:''}>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">What is GPT?</a></li>
+                  <li><a href="#">Open AI</a></li>
+                  <li><a href="#">Case Studies</a></li>
+                  <li><a href="#">Library</a></li>
+                  <li><a href="#">Sign in</a></li>
             </ul>)}
            
         </nav>
